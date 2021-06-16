5925709790d37a361b027e1e58df815eb3e09c43
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-16 16:30:04 +0300

    feat: added solution to exercise 1 of part 1

---
+++ part1/course_info/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.content.map((item, idx) => {
+        return <p key={idx}>{item.part} {item.exercises}</p>;
+      })}
+    </>
+  );
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>
+        Number of exercises {props.content.reduce((acc, val) => {
+          return acc + val.exercises;
+        }, 0)}
+      </p>
+    </>
+  );
+}
+
+const App = () => {
+  const course = 'Half stack application development'
+  const content = [
+    {
+      part: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      part: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      part: 'State of a component',
+      exercises: 14
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={content} />
+      <Total content={content} />
+    </div>
+  );
+}
+
+export default App;
